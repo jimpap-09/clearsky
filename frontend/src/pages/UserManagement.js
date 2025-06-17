6b8eba24ce9b50369d6a93ffc8b7768b92167463
jimlee <el19643@mail.ntua.gr>
2025-06-17 14:27:55 +0300

    commit before pull

--- frontend/src/pages/UserManagement.js
+++ frontend/src/pages/UserManagement.js
@@ -1,24 +1,31 @@
-import {useState} from 'react';
+import { useEffect, useState } from 'react';
+import MessageArea from '../components/ui/MessageArea';
 import UserDropDown from '../components/ui/UserDropDown';
 
 export default function UserManagement() {
-    const options = ['instructor', 'student', 'representative']
+    const options = ['instructor', 'student', 'representative'];
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [id, setId] = useState('');
+    const [action, setAction] = useState('');
+
+    useEffect(()=>{
+        if(action !== 'student') setId('');
+    },[action]);
 
     return (
         <>
         <div className="main-container">
             <h2 className='main-container-header'>Users</h2>
-            <form>
+            <form className='main-form'>
                 <div className="label-container">
-                    <label>type:</label>
-                    <UserDropDown options={options} start={'institution representative'}/>
+                    <label className='main-label'>type:</label>
+                    <UserDropDown options={options} start={'institution representative'} setAction={setAction}/>
                 </div>
                 <div className="label-container">
-                    <label>Username:</label>
+                    <label className='main-label'>Username:</label>
                     <input
+                        className='main-input'
                         type="text"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
@@ -26,8 +33,9 @@ export default function UserManagement() {
                     />
                 </div>
                 <div className="label-container">
-                    <label>Password:</label>
+                    <label className='main-label'>Password:</label>
                     <input
+                        className='main-input'
                         type="text"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
@@ -35,35 +43,30 @@ export default function UserManagement() {
                     />
                 </div>
                 <div className="label-container">
-                    <label>id:</label>
+                    <label className='main-label'>id:</label>
                     <input
+                        className='main-input'
                         type="text"
                         value={id}
                         onChange={(e) => setId(e.target.value)}
+                        disabled={action !== 'student'}
                     />
                 </div>
                 <div className='usermngmnt-btn-container'>
                     <div className='submit-btn'>
-                        <button type="submit">
+                        <button className='main-button' type="submit">
                             Add user
                         </button>
                     </div>
                     <div className='submit-btn'>
-                        <button type="button">
+                        <button className='main-button' type="button">
                             Change password
                         </button>
                     </div>
                 </div>
             </form>
         </div>
-        <div className='message-area'>
-            <div className='main-container'>
-                <h2 className='main-container-header'>Message area</h2>
-                <div className='message-text'>
-                    <p>write your message here</p>
-                </div>
-            </div>
-        </div>
+        <MessageArea/>
         </>
     )
 }
